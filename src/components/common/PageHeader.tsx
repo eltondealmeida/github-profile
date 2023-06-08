25f103fa09ad8489d290e6ac8650c10ac9f4a6d8
Elton de Almeida <elton_de_almeida@hotmail.com>
2023-06-08 00:23:56 -0300

    refactor: Refactor PageHeader using styled-components, implement offcanvas menu for mobile responsiveness, and create Profile component.

--- src/components/common/PageHeader.tsx
+++ src/components/common/PageHeader.tsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import {
   Navbar,
   Nav,
   Container,
   Form,
-  InputGroup,
   Button,
+  Offcanvas,
 } from "react-bootstrap";
-import { BsSearch } from "react-icons/bs";
+import { BsSearch, BsList } from "react-icons/bs";
+import styled from "styled-components";
 import githubLogo from "../../assets/img/github.png";
 import githubText from "../../assets/img/github-text.png";
 import { useFormContext } from "react-hook-form";
@@ -18,6 +20,7 @@ export interface PageHeaderProps {
 
 export function PageHeader({ children }: PageHeaderProps): JSX.Element {
   const { register, watch, setValue, handleSubmit } = useFormContext<User>();
+  const [showOffcanvas, setShowOffcanvas] = useState(false);
 
   const onSearchSubmit = async () => {
     setValue("isLoading", true);
@@ -47,45 +50,158 @@ export function PageHeader({ children }: PageHeaderProps): JSX.Element {
       console.error("Error:", error);
     } finally {
       setValue("isLoading", false);
+      if (showOffcanvas) {
+        setShowOffcanvas(false);
+      }
     }
   };
+
+  function handleKeyPress(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (e.key === "Enter") {
+      handleSubmit(onSearchSubmit)();
+    }
+  }
+
+  const LogoContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin-left: 60px;
+  `;
+
+  const LogoImage = styled.img`
+    width: 24px;
+    height: 23px;
+    margin-right: 8px;
+  `;
+
+  const LogoText = styled.img`
+    width: 90px;
+    height: 40px;
+  `;
+
+  const ProfileLink = styled(Nav.Link)`
+    color: #fff;
+  `;
+
+  const SearchForm = styled(Form)`
+    display: flex;
+    align-items: center;
+  `;
+
+  const SearchInput = styled(Form.Control)`
+    border-radius: 4px;
+    width: 150px;
+    font-size: 12px;
+
+    &::placeholder {
+      font-size: 12px;
+    }
+  `;
+
+  const SearchButton = styled(Button)`
+    &&& {
+      background-color: transparent;
+      border: none;
+      cursor: pointer;
+      transition: background-color 0.3s;
+
+      &:focus,
+      &:active {
+        background-color: transparent;
+      }
+
+      &:active {
+        background-color: rgba(0, 0, 0, 0.1);
+      }
+    }
+  `;
+
+  const SearchOffcanvasButton = styled(Button)`
+    &&& {
+      background-color: transparent;
+      border: none;
+      cursor: pointer;
+      transition: background-color 0.3s;
+
+      &:focus,
+      &:active {
+        background-color: transparent;
+      }
+
+      &:active {
+        background-color: rgba(0, 0, 0, 0.1);
+      }
+
+      svg {
+        color: #888;
+      }
+    }
+  `;
 
   return (
     <>
       <Navbar bg="dark" variant="dark" className="mb-0">
         <Container className="m-2">
-          <Navbar.Brand href="home" style={{ marginLeft: "20%" }}>
-            <img alt="Github logo" src={githubLogo} width="24" height="23" />
-            <img
-              alt="Github text"
-              src={githubText}
-              width="85"
-              height="40"
-              className="ms-2"
-            />
-          </Navbar.Brand>
-          <Navbar.Brand href="home" className="fw-bold">
-            /
-          </Navbar.Brand>
-          <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: "100px" }}>
-            <Nav.Link href="home">Profile</Nav.Link>
+          <LogoContainer>
+            <LogoImage alt="Github logo" src={githubLogo} />
+            <LogoText alt="Github text" src={githubText} />
+            <Nav>
+              <Nav.Item>
+                <Nav.Link href="/" className="fw-bold">
+                  /
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <ProfileLink href="home">Profile</ProfileLink>
+              </Nav.Item>
+            </Nav>
+          </LogoContainer>
+          <Nav className="ms-auto d-lg-none text-end">
+            <Button
+              variant="outline-secondary"
+              onClick={() => setShowOffcanvas(!showOffcanvas)}
+            >
+              <BsList />
+            </Button>
           </Nav>
-
-          <Form onSubmit={handleSubmit(onSearchSubmit)}>
-            <InputGroup size="sm">
-              <Form.Control
-                type="search"
-                placeholder="Search username"
-                aria-label="Search"
-                {...register("login")}
-              />
-              <Button variant="outline-secondary" type="submit">
-                <BsSearch />
-              </Button>
-            </InputGroup>
-          </Form>
+          <SearchForm
+            onSubmit={handleSubmit(onSearchSubmit)}
+            className="d-none d-lg-flex"
+          >
+            <SearchInput
+              type="search"
+              placeholder="Search username"
+              {...register("login")}
+              onKeyPress={handleKeyPress}
+            />
+            <SearchButton type="submit">
+              <BsSearch />
+            </SearchButton>
+          </SearchForm>
         </Container>
       </Navbar>
+      <Offcanvas
+        show={showOffcanvas}
+        onHide={() => setShowOffcanvas(false)}
+        placement="end"
+      >
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Menu</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <SearchForm onSubmit={handleSubmit(onSearchSubmit)}>
+            <SearchInput
+              type="search"
+              placeholder="Search username"
+              {...register("login")}
+              onKeyPress={handleKeyPress}
+            />
+            <SearchOffcanvasButton type="submit">
+              <BsSearch className="search-icon-black" />
+            </SearchOffcanvasButton>
+          </SearchForm>
+        </Offcanvas.Body>
+      </Offcanvas>
       <div className="m-3">{children}</div>
     </>
   );
--- src/components/pages/home/HomePage.tsx
+++ src/components/pages/home/HomePage.tsx
@@ -1,22 +1,23 @@
+import { Spinner } from "react-bootstrap";
 import { useFormContext } from "react-hook-form";
 import { PageHeader } from "../../common/PageHeader";
 import { User } from "../../../types/User";
-import { Spinner } from "react-bootstrap";
+import { Profile } from "../../common/Profile";
 
 export default function HomePage() {
   const { watch } = useFormContext<User>();
 
   return (
     <PageHeader>
-      <div className="text-center justify-content-center">
-        {watch("isLoading") ? (
+      {watch("isLoading") ? (
+        <div className="text-center justify-content-center">
           <Spinner animation="border" />
-        ) : watch("statusSearch")?.length > 0 ? (
-          <h5>{watch("statusSearch")}</h5>
-        ) : (
-          <p>{watch("name")}</p>
-        )}
-      </div>
+        </div>
+      ) : watch("statusSearch")?.length > 0 ? (
+        <h5 className="text-center text-danger">{watch("statusSearch")}</h5>
+      ) : (
+        <Profile />
+      )}
     </PageHeader>
   );
 }
